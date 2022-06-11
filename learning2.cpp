db2e0b076971e64f8c936e9feece99b6e1636527
Md Faraz Ahmad <farazpatna7881@gmail.com>
2022-06-11 16:41:21 +0530

    origin

--- learning2.cpp
+++ learning2.cpp
@@ -4,35 +4,63 @@ using namespace std;
 
 int main() {
 
-  int age;
-  cin >> age;
-  cout << "Hi, Ur age is .... " << age;
+//   int age;
+//   cin >> age;
+//   cout << "Hi, Ur age is .... " << age;
 
-  float height;
-  cin >> height;
-  cout << "Hi, You are.. " << height << " meter tall";
+//   float height;
+//   cin >> height;
+//   cout << "Hi, You are.. " << height << " meter tall";
 
-  char initial;
-  cin >> initial;
-  cout << "Hi, So tne initial of your name is " << initial;
+//   char initial;
+//   cin >> initial;
+//   cout << "Hi, So tne initial of your name is " << initial;
 
-int age;
-char initial;
-float height;
+// int age;
+// char initial;
+// float height;
 
-cout << "Please enter ur age: " << endl;
-cin >> age;
-cout << "Please enter ur initial: " << endl;
-cin >> initial;
-cout << "Please enter ur height: " << endl;
+// cout << "Please enter ur age: " << endl;
+// cin >> age;
+// cout << "Please enter ur initial: " << endl;
+// cin >> initial;
+// cout << "Please enter ur height: " << endl;
+// cin >> height;
+
+// cout << initial << " of " << age << " age and " << height << "m height.";
+
+// int a, b;
+// cin >> a >> b;
+// cout << "Value of a is " << a << endl;
+// cout << "Value of b is " << a << endl;
+
+// Increament and Decreament
+
+int a, b, c, d;
+a = 500, b = 200;
+
+a++;
+a++;
+b++;
+a--;
+;
+cout << "Value of a is: " << a << endl;
+cout << "Value of b is: " << b << endl;
+
+// Area Of Triangle
+
+int height, base;
+float area;
+
+cout << "Enter the height of Triangle: " <<endl;
 cin >> height;
+cout << "Enter the base of Triangle: " <<endl;
+cin >> base;
 
-cout << initial << " of " << age << " age and " << height << "m height.";
+area = height * base / 2;
+cout << "Area of Triangle is: " << area << endl;
 
-int a, b;
-cin >> a >> b;
-cout << "Value of a is " << a << endl;
-cout << "Value of b is " << a << endl;
+
 
 
     return 0;
