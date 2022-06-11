60aed695e66faa94c2f107121e4af49d4c13fc3a
Md Faraz Ahmad <farazpatna7881@gmail.com>
2022-06-11 15:43:31 +0530

    first

---
+++ learning2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int age;
+  cin >> age;
+  cout << "Hi, Ur age is .... " << age;
+
+  float height;
+  cin >> height;
+  cout << "Hi, You are.. " << height << " meter tall";
+
+  char initial;
+  cin >> initial;
+  cout << "Hi, So tne initial of your name is " << initial;
+
+int age;
+char initial;
+float height;
+
+cout << "Please enter ur age: " << endl;
+cin >> age;
+cout << "Please enter ur initial: " << endl;
+cin >> initial;
+cout << "Please enter ur height: " << endl;
+cin >> height;
+
+cout << initial << " of " << age << " age and " << height << "m height.";
+
+int a, b;
+cin >> a >> b;
+cout << "Value of a is " << a << endl;
+cout << "Value of b is " << a << endl;
+
+
+    return 0;
+
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cout << "Hello, World!" << endl;
+
+    int a, b;
+cin >> a >> b;
+cout << "Value of a is " << a << endl;
+cout << "Value of b25 is " << a << endl;
+
+    return 0;
+}
+
+
+
